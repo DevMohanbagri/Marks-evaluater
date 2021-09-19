400f8ce82adc56ed27c10823f59350257f4167b1
DevMohanbagri <73389635+DevMohanbagri@users.noreply.github.com>
2021-09-19 23:33:18 +0530

    Add files via upload

---
+++ word.py
@@ -0,0 +1,8 @@
+TestMarks= int(input("Enter your marks here "))
+
+if(TestMarks<20):
+    print("You are fail")
+elif(TestMarks==50):
+    print("You got 50/50!") 
+else:
+    print("You can do better")
